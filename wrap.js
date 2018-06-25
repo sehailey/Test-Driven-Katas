af1baf6743f928bea230effc9a80d664c960a1b8
sehailey <sarah.elizabeth.hailey@gmail.com>
2018-06-25 12:20:00 -0400

    commit 2

--- wrap.js
+++ wrap.js
@@ -1,5 +1,16 @@
-function wrap (str, num) {
+function wrap(str, num) {
+    var arr = str.split(' ');
+    if (arr.map(elem => elem.length > num).reduce((a, b) => a || b)) {
+        throw new Error('one of the words is too long!');
+    } else {
+        let begin = 0;
+        while (begin !== str.lastIndexOf(' ')) {
+            let chunk = str.slice(begin, num);
+            newStr += chunk;
+        }
+    }
 
+    return newStr;
 }
 
 module.exports = wrap;
